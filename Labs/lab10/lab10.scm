d023ee6acc7cab21079fe2ec5a7588a20a282410
UncleXuu <ssyxx1@nottingham.ac.uk>
2024-05-09 19:12:08 +0100

    lab10 finished

---
+++ Labs/lab10/lab10.scm
@@ -0,0 +1,31 @@
+(define (over-or-under num1 num2) 
+    (if (< num1 num2) 
+        -1
+        (if (> num1 num2) 1 0
+        )
+    )
+)
+
+(define (make-adder num)
+    (lambda (inc) (+ num inc))
+)
+
+(define (composed f g) 
+    (lambda (x)
+        (f
+            (g x)
+        )
+    )
+)
+
+(define lst 
+    (cons (cons 1 nil) 
+        (cons 2 (cons (cons 3 (cons 4 nil)) (cons 5 nil))))
+)
+
+(define (remove item lst)
+    (filter
+        (lambda (x) (not (= x item)))
+        lst
+    )
+)
