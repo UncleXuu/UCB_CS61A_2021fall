0f45c2f2d5c0735e26c3ccb51cc3a74d8bbf066d
UncleXuu <ssyxx1@nottingham.ac.uk>
2024-05-15 20:23:29 +0100

    hw09 finished

--- Homeworks/hw09/hw09.py
+++ Homeworks/hw09/hw09.py
@@ -19,7 +19,8 @@ def roman_numerals(text):
     >>> roman_numerals("she loves ALL editors equally.")
     []
     """
-    return re.findall(__________, text)
+    #从头到尾都是方括号里的数
+    return re.findall(r'\b[IVXLCDM]+\b', text)
 
 
 import re
@@ -43,7 +44,7 @@ def cs_classes(post):
     >>> cs_classes("What are some good CS upper division courses? I was thinking about CS 161 or CS 169a")
     True
     """
-    return bool(re.search(__________, post))
+    return bool(re.search(r'\b(CS|cs).*[ABCabc]\b', post))
 
 
 import re
@@ -60,7 +61,7 @@ def match_time(text):
     >>> match_time("At 2:00 I pinged 127.0.0.1:80.")
     ['2:00']
     """
-    return re.findall(__________, text)
+    return re.findall(r'[0-9][0-9]?:[0-6][0-9][apmAPM]*', text)
 
 
 import re
@@ -79,7 +80,8 @@ def area_codes(text):
     >>> area_codes("no matches for 12 3456 7890 or 09876-54321")
     []
     """
-    return re.findall(__________, text)
+    # (?: ...) is non-capturing group, and the group you want to output should into a individual group ()
+    return re.findall(r'([\d]{3})(?:\)?[-\s]?[\d]{3}[-\s]?[\d]{4}\b)', text)
 
 
 def most_common_code(text):
@@ -97,4 +99,5 @@ def most_common_code(text):
     >>> most_common_code(input_text)
     '123'
     """
-    "*** YOUR CODE HERE ***"
+    list = re.findall(r'([\d]{3})(?:\)?[-\s]?[\d]{3}[-\s]?[\d]{4})',text)
+    return list[0]
